321f6e91366dbc669097cd32fa68625dd8e908d6
Amber Chen <33996675+axychen@users.noreply.github.com>
2019-07-30 01:58:07 -0400

    Update card.h

--- card.h
+++ card.h
@@ -7,15 +7,14 @@ class Card{
 	std::string name;
 	int cost;
 
-
-	public:
+     public:
 	int atk;
 	int def;
 	Card(std::string name, int cost);
 	virtual ~Card() = default;
 	std::string getName() const;
-	int getCost() const;
 	void setName(std::string name);
+	int getCost() const;
 	void setCost(int cost);
 	int getAtk();
 	void setAtk(int atk);
